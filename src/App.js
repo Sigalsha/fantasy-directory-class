0f2e478b24739bf3a5f7124d4ec7e80cdc494cd0
Sigal Shalit Manor <sigal.shalit.manor@gmail.com>
2018-09-24 20:21:07 +0300

    adding links,params and match

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home.js';
+import About from './components/About.js';
+import Fentities from './components/Fentities.js';
+import Fentity from './components/Fentity.js';
+
 
 class App extends Component {
   constructor() {
@@ -7,7 +13,8 @@ class App extends Component {
     this.state = {
       wizards: [
         { name: "Merlin", power: "Wisdom", other: "Helped King Arthur", imgUrl: "https://tinyurl.com/merlin-image" },
-        { name: "Morgana Le Fay", power: "Forces of Nature", other: "Trapped Merlin in a cave for eternity", imgUrl: "https://tinyurl.com/morgana-image" },
+      { name: "Morgana Le Fay", power: "Forces of Nature", other: `Trapped ${<Link to="/directory/wizards/merlin">Merlin</Link>} in a cave for eternity`, 
+      imgUrl: "https://tinyurl.com/morgana-image" },
         { name: "Gandalf", power: "Plot Convenience", other: "Once broke a bridge", imgUrl: "https://tinyurl.com/gandalf-img" }
       ],
       bestiary: [
@@ -21,15 +28,19 @@ class App extends Component {
   render() {
     const state = this.state
     return (
-      <div className="App">
-        <div id="home-background"></div>
-        <div id="main-links">
-          {/* Main Links */}
+      <Router>
+        <div className="App">
+          <div id="home-background"></div>
+          <div id="main-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </div>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact render={() => <About items={Object.keys(state)} />} />
+            <Route path="/directory/:fentities" exact render={({ match }) => <Fentities match={match} state={state} />}/>
+            <Route path="/directory/:fentities/:name" exact render={({ match }) => <Fentity match={match} state={state}/>}/>
         </div>
-        {/* Routes go here v */}
-
-        {/* Routes go here ^ */}
-      </div>
+      </Router>
     );
   }
 }
--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/fentity.css'
-
 
 class Fentity extends Component {
     render() {
+        // const merlinLink = <Link to="/directory/wizards/merlin">Merlin</Link>
+        // const morganaOther = `Trapped ${merlinLink} in a cave for eternity` 
+        const match = this.props.match
+        const fentitiesCategory = match.params.fentities
+        const name = match.params.name
+        const fentity = this.props.state[fentitiesCategory].filter(f => { 
+            return f.name.toLowerCase() === name.toLowerCase() })[0]
         return (
             <div id="creature-container">
                 <h1>{fentity.name}</h1>
                 <img src={fentity.imgUrl} alt=""/>
                 <div className="title">Power:</div>
                 <div className="power text"> {fentity.power}</div>
-                <div className="other text">{fentity.other}</div>
+                <div className="other text">
+                    {/* {fentity.other.includes("Merlin") ? morganaOther : fentity.other} */}
+                    {fentity.other}
+                    </div>
             </div>
         )
     }
